62b9f5e8971bfaa03990174a1de75590c459298a
kirkya99 <fabianschotte@outlook.com>
2025-06-09 01:33:07 +0200

    added notebook for final task with gru 1 and 2 and comparision

--- final_task_kaggle_rnn/models.py
+++
@@ -1,9 +0,0 @@
-import keras
-
-
-def model_1(vocab_size, embedding_dim, rnn_units):
-    embedding = keras.layers.Embedding(input_dim=vocab_size, output_dim=embedding_dim)
-    gru = keras.layers.GRU(units=rnn_units, return_sequences=True, return_state=True)(embedding)
-    outputs = keras.layers.Dense(units=vocab_size, activation='softmax')(gru)
-
-    return keras.Model(inputs=vocab_size, outputs=outputs)
