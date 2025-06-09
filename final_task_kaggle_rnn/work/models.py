1f5b1fd0e0680cf9fd7fd650d48d6e40467d8e2c
kirkya99 <fabianschotte@outlook.com>
2025-06-09 19:48:39 +0200

    added code for gru_1, gru_2, lstm & added code for text generation

--- final_task_kaggle_rnn/work/models.py
+++ final_task_kaggle_rnn/work/models.py
@@ -22,18 +22,18 @@ def get_gru_model_2(vocab_size, embedding_dim, rnn_units):
 def get_lstm_model_1(vocab_size, embedding_dim, rnn_units):
     inputs = keras.layers.Input(shape=(None,), dtype='int32', name='input_tokens')
     embedding = keras.layers.Embedding(input_dim=vocab_size, output_dim=embedding_dim)(inputs)
-    gru, gru_state = keras.layers.LSTM(units=rnn_units, return_sequences=True, return_state=True)(embedding)
-    gru, gru_state = keras.layers.LSTM(units=rnn_units, return_sequences=True, return_state=True)(gru)
-    outputs = keras.layers.Dense(units=vocab_size, activation='softmax')(gru)
+    lstm, hidden_state_1, cell_state_1 = keras.layers.LSTM(units=rnn_units, return_sequences=True, return_state=True)(embedding)
+    lstm, hidden_state_2, cell_state_2 = keras.layers.LSTM(units=rnn_units, return_sequences=True, return_state=True)(lstm)
+    outputs = keras.layers.Dense(units=vocab_size, activation='softmax')(lstm)
 
     return keras.Model(inputs=inputs, outputs=outputs)
 
 def get_lstm_model_2(vocab_size, embedding_dim, rnn_units):
     inputs = keras.layers.Input(shape=(None,), dtype='int32', name='input_tokens')
     embedding = keras.layers.Embedding(input_dim=vocab_size, output_dim=embedding_dim)(inputs)
-    gru, gru_state = keras.layers.LSTM(units=rnn_units, return_sequences=True, return_state=True)(embedding)
-    dropout = keras.layers.Dropout(0.2)(gru)
-    gru, gru_state = keras.layers.LSTM(units=rnn_units, return_sequences=True, return_state=True)(dropout)
-    outputs = keras.layers.Dense(units=vocab_size, activation='softmax')(gru)
+    lstm, hidden_state_1, cell_state_1 = keras.layers.LSTM(units=rnn_units, return_sequences=True, return_state=True)(embedding)
+    dropout = keras.layers.Dropout(0.2)(lstm)
+    lstm, hidden_state_2, cell_state_2 = keras.layers.LSTM(units=rnn_units, return_sequences=True, return_state=True)(dropout)
+    outputs = keras.layers.Dense(units=vocab_size, activation='softmax')(lstm)
 
     return keras.Model(inputs=inputs, outputs=outputs)
